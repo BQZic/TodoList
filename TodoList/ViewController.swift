1551e9f890c4beecb961bd04558b345f8111c816
Beiqi Zou <zoub@rpi.edu>
2018-12-24 22:15:08 +0800

    Add keyboard animation

--- TodoList/ViewController.swift
+++ TodoList/ViewController.swift
@@ -10,11 +10,28 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBAction func addNewItem(_ sender: Any, forEvent event: UIEvent) {
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-
+    
+    func keyBoardWillHide(note:NSNotification)
+    {
+        let userInfo  = note.userInfo as! NSDictionary
+        let duration = (userInfo[UIResponder.keyboardAnimationDurationUserInfoKey] as! NSNumber).doubleValue
+        let animations:(() -> Void) = {
+            self.keyBaordView.transform = CGAffineTransformIdentity
+        }
+        
+        if duration > 0 {
+            let options = UIView.AnimationOptions(UInt((userInfo[UIKeyboardAnimationCurveUserInfoKey] as! NSNumber).integerValue << 16))
+            
+            UIView.animateWithDuration(duration, delay: 0, options:options, animations: animations, completion: nil)
+        }else{
+            animations()
+        }
+    }
 }
 
