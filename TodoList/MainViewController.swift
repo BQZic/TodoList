bb036d9bdf22da9fd78a5a635fac2cdd6febd2fd
Haoyun Zhu <zhuh12@rpi.edu>
2019-01-02 17:11:38 +0800

    replace the navigation VC with a container VC

---
+++ TodoList/MainViewController.swift
@@ -0,0 +1,30 @@
+//
+//  MainViewController.swift
+//  TodoList
+//
+//  Created by Haoyun Zhu on 2019/1/2.
+//  Copyright © 2019 Haoyun Zhu. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
